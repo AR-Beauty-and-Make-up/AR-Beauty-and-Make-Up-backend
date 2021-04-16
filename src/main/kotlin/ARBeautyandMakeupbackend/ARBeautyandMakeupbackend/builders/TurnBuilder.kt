c10a5f6941a2be5b196b9cb1f71e2b5a47d6bc36
Luciana <luliialonso@hotmail.com>
2021-04-16 11:58:29 -0300

    agrego numero de contacto de cliente en turno

--- src/main/kotlin/ARBeautyandMakeupbackend/ARBeautyandMakeupbackend/builders/TurnBuilder.kt
+++ src/main/kotlin/ARBeautyandMakeupbackend/ARBeautyandMakeupbackend/builders/TurnBuilder.kt
@@ -8,6 +8,7 @@ class TurnBuilder {
     private var client: String = "Lucas Avalos"
     private var date: LocalDateTime = LocalDateTime.of(2021, 4, 20, 16,0)
     private var service: String = "Masajes"
+    private var contactNumber: Int = 1168686868
 
     companion object {
         fun aTurn(): TurnBuilder {
@@ -16,6 +17,6 @@ class TurnBuilder {
     }
 
     fun build(): Turn {
-        return Turn(this.client, this.date, this.service)
+        return Turn(this.client, this.date, this.service, this.contactNumber)
     }
 }
