e4c568d11aa385487b2e8b60bcbdbf68836681d1
Luciana <luliialonso@hotmail.com>
2021-05-22 22:46:33 -0300

    Agrego test de repositorio de usuario

---
+++ src/test/kotlin/ARBeautyandMakeupbackend/ARBeautyandMakeupbackend/repository/UserRepositoryTest.kt
@@ -0,0 +1,41 @@
+package ARBeautyandMakeupbackend.ARBeautyandMakeupbackend.repository
+
+import ARBeautyandMakeupbackend.ARBeautyandMakeupbackend.builders.UserBuilder
+import ARBeautyandMakeupbackend.ARBeautyandMakeupbackend.model.user.Client
+import ARBeautyandMakeupbackend.ARBeautyandMakeupbackend.model.user.User
+import ARBeautyandMakeupbackend.ARBeautyandMakeupbackend.persistence.UserRepository
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@DataJpaTest
+class UserRepositoryTest {
+
+    @Autowired
+    lateinit var userRepository: UserRepository
+
+    @Test
+    fun testUserRepositorySaveAndRetrievedAClientUser(){
+        val clientUser: User = UserBuilder.aUser().build()
+        userRepository.save(clientUser)
+
+        val retrievedClienUser: User = userRepository.findById(clientUser.id!!).get()
+
+        Assert.assertEquals(clientUser.fullname, retrievedClienUser.fullname)
+    }
+
+    @Test
+    fun testUserRepositorySaveAndRetrievedAnAdminUser(){
+        val adminUser: User = UserBuilder.aUser().buildAdmin()
+        userRepository.save(adminUser)
+
+        val retrievedAdminUser = userRepository.findById(adminUser.id!!).get()
+
+        Assert.assertEquals(adminUser.fullname, retrievedAdminUser.fullname)
+        Assert.assertTrue(retrievedAdminUser.admin)
+    }
+}
