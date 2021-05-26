d71c2fd69c7e255633676fb63e848d82e35d356d
Luciana <luliialonso@hotmail.com>
2021-05-26 18:59:03 -0300

    Agrego test en controller para obetener todas las categorias de productos disponibles

--- src/test/kotlin/ARBeautyandMakeupbackend/ARBeautyandMakeupbackend/controllers/ProductControllerTest.kt
+++ src/test/kotlin/ARBeautyandMakeupbackend/ARBeautyandMakeupbackend/controllers/ProductControllerTest.kt
@@ -1,7 +1,5 @@
 package ARBeautyandMakeupbackend.ARBeautyandMakeupbackend.controllers
 
-import ARBeautyandMakeupbackend.ARBeautyandMakeupbackend.builders.ProductBuilder
-import ARBeautyandMakeupbackend.ARBeautyandMakeupbackend.model.category.Category
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
@@ -25,6 +23,7 @@ class ProductControllerTest {
     @Test
     fun ifWeAskForProductsWeGetTheAllProducts(){
         //Como podria testear el contenido? guardar los productos en una lista pidiendoselos al service?
+        //Como puedo chequear la cantidad de elementos de la lista retornada??
         mockMvc.perform(MockMvcRequestBuilders.get("/products")
             .contentType(MediaType.APPLICATION_JSON))
             .andExpect(MockMvcResultMatchers.status().isOk)
@@ -32,7 +31,7 @@ class ProductControllerTest {
     }
 
     @Test
-    fun ifWeAskForProductsOfASpecificCategoryWeGetTheoOnlyProductsWithThatCategory(){
+    fun ifWeAskForProductsOfASpecificCategoryWeGetAllProductsWithThatCategory(){
         mockMvc.perform(MockMvcRequestBuilders.get("/products/Cremas")
             .contentType(MediaType.APPLICATION_JSON))
             .andExpect(MockMvcResultMatchers.status().isOk)
@@ -42,5 +41,15 @@ class ProductControllerTest {
             .andExpect(MockMvcResultMatchers.jsonPath("$.[2].category").value("Cremas"))
             .andExpect(MockMvcResultMatchers.jsonPath("$.[3].category").value("Cremas"))
             .andExpect(MockMvcResultMatchers.jsonPath("$.[4].category").value("Cremas"))
+    }
+
+    @Test
+    fun ifWeAskForAllCateogriesOfProductsWeGetAllCategoriesAnd200Satus(){
+        mockMvc.perform(MockMvcRequestBuilders.get("/categories")
+            .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(MockMvcResultMatchers.status().isOk)
+            .andExpect(MockMvcResultMatchers.jsonPath("$").isArray)
+            .andExpect(MockMvcResultMatchers.jsonPath("$.[0]").value("Cremas"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.[1]").value("Maquillaje"))
     }
 }
