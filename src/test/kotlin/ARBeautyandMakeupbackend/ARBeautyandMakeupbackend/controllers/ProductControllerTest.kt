85beb1409078cc199d1322e9b1eae499db25f690
Luciana <luliialonso@hotmail.com>
2021-05-26 18:31:10 -0300

    Agrego test de controller de productos para obtener los productos de una categoria

--- src/test/kotlin/ARBeautyandMakeupbackend/ARBeautyandMakeupbackend/controllers/ProductControllerTest.kt
+++ src/test/kotlin/ARBeautyandMakeupbackend/ARBeautyandMakeupbackend/controllers/ProductControllerTest.kt
@@ -2,14 +2,11 @@ package ARBeautyandMakeupbackend.ARBeautyandMakeupbackend.controllers
 
 import ARBeautyandMakeupbackend.ARBeautyandMakeupbackend.builders.ProductBuilder
 import ARBeautyandMakeupbackend.ARBeautyandMakeupbackend.model.category.Category
-import ARBeautyandMakeupbackend.ARBeautyandMakeupbackend.services.ProductService
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.Mockito.`when`
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.http.MediaType
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.web.servlet.MockMvc
@@ -22,27 +19,21 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 @AutoConfigureMockMvc
 class ProductControllerTest {
 
-    @MockBean
-    lateinit var productServiceMock: ProductService
-
     @Autowired
     lateinit var mockMvc: MockMvc
 
     @Test
     fun ifWeAskForProductsWeGetTheAllProducts(){
+        //Como podria testear el contenido? guardar los productos en una lista pidiendoselos al service?
         mockMvc.perform(MockMvcRequestBuilders.get("/products")
             .contentType(MediaType.APPLICATION_JSON))
             .andExpect(MockMvcResultMatchers.status().isOk)
     }
 
-    /*
     @Test
     fun ifWeAskForProductsOfASpecificCategoryWeGetTheoOnlyProductsWithThatCategory(){
-        var allProducts = ProductBuilder.productListWithDifrentCategory()
-        `when`(productServiceMock.findAllByACategory(Category.Cremas)).thenReturn(allProducts)
-
-        mockMvc.perform(MockMvcRequestBuilders.get("/products/Cremas"))
-                .andExpect(MockMvcResultMatchers.status().isOk)
+        mockMvc.perform(MockMvcRequestBuilders.get("/products/Cremas")
+            .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(MockMvcResultMatchers.status().isOk)
     }
-    */
 }
