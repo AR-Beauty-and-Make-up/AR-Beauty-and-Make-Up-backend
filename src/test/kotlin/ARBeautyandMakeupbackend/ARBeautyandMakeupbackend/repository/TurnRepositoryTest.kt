d8c216b430fe960e2f8bae89f88e9843824bca75
Luciana <luliialonso@hotmail.com>
2021-06-11 18:56:23 -0300

    Agrego test de repositorio para updatear un usuario

--- src/test/kotlin/ARBeautyandMakeupbackend/ARBeautyandMakeupbackend/repository/TurnRepositoryTest.kt
+++ src/test/kotlin/ARBeautyandMakeupbackend/ARBeautyandMakeupbackend/repository/TurnRepositoryTest.kt
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.test.context.junit4.SpringRunner
 import java.time.LocalDate
-import java.time.LocalDateTime
 
 @RunWith(SpringRunner::class)
 @DataJpaTest
